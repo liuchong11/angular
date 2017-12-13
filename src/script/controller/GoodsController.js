73dc69744f49a549eaadd06e7ded4996e419845b
我的天 <1052639078@qq.com>
2017-12-13 11:19:09 +0800

    one

---
+++ src/script/controller/GoodsController.js
@@ -0,0 +1,14 @@
+angular.module('controllers')
+.controller('GoodsController',[
+    '$scope',
+    'route',
+    '$routeParams',
+    'GoodsService',
+    function(
+        $scope,
+        $route,
+        $routeParams,
+        GoodsService
+    ){
+        $scope.goodsList = GoodsService.fetchGoodsList();
+    }])
